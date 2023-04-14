d7b5ee17cecb4d58f91b556145a127788379bec4
DreamCat04 <thierry@kellyburger.com>
2023-04-14 14:34:58 +0200

    Included insurance amount field

--- RechnerProgramm/src/ch/bbw/versicherungsrechner/Rechner/Rechner.java
+++ RechnerProgramm/src/ch/bbw/versicherungsrechner/Rechner/Rechner.java
@@ -1,6 +1,7 @@
 package ch.bbw.versicherungsrechner.Rechner;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
@@ -8,27 +9,34 @@ import java.text.SimpleDateFormat;
 public class Rechner extends JPanel implements ActionListener {
 	public Rechner(){
 		JFrame frame = new JFrame("Schadensrechner");
-		JPanel versSumme = new JPanel();
+		JPanel versSummeModell = new JPanel();
 		JPanel quadratMeterModell = new JPanel();
 		JLabel damageDesc = new JLabel("Geben Sie hier ihre Schadenssumme ein");
 		JTextField damageAmount = new JTextField();
+		JLabel insuranceAmountDesc = new JLabel("Geben Sie hier den Versicherungswert Ihres Hauses ein");
+		JTextField insuranceAmount = new JTextField();
 		JLabel sumDesc = new JLabel("Geben Sie hier Ihre Versicherungssumme ein");
 		JTextField sumAmount = new JTextField();
-		JLabel output = new JLabel("Ihre Schadensersatzsumme beträgt: ");
+		JLabel insurancePayout = new JLabel("Ihre Schadensersatz beträgt: ");
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setSize(640, 360);
+		frame.add(versSummeModell);
 		
-		versSumme.setVisible(true);
-		versSumme.setLayout(new BoxLayout(versSumme, BoxLayout.Y_AXIS));
-		versSumme.add(damageDesc);
-		versSumme.add(damageAmount);
-		versSumme.add(sumDesc);
-		versSumme.add(sumAmount);
-		versSumme.add(output);
+		versSummeModell.setVisible(true);
+		versSummeModell.setLayout(new BoxLayout(versSummeModell, BoxLayout.PAGE_AXIS));
+		versSummeModell.add(damageDesc);
+		versSummeModell.add(damageAmount);
+		versSummeModell.add(insuranceAmountDesc);
+		versSummeModell.add(insuranceAmount);
+		versSummeModell.add(sumDesc);
+		versSummeModell.add(sumAmount);
+		versSummeModell.add(insurancePayout);
 		
-		frame.add(versSumme);
+		damageAmount.setMaximumSize(new Dimension(640, 50));
+		sumAmount.setMaximumSize(new Dimension(640, 50));
+		insuranceAmount.setMaximumSize(new Dimension(640, 50));
 		
 		
 	}
