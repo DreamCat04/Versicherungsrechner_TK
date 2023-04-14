c4e6c14400ef428c8603b04ab59b8bda2a09bff7
DreamCat04 <thierry@kellyburger.com>
2023-04-14 15:47:57 +0200

    Add navbar

--- rechner/src/App.js
+++ rechner/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <nav>
+          <a href='#'>Versicherungssummenmodell</a>
+          <a href='#'>Quadratmetermodell</a>
+        </nav>
+      </header>
   <p>Da kommt die App hinein</p>
     </div>
   );
